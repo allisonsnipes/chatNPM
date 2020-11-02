8a7749efd7f2065661b84ef5722d451df8007d90
Allison Snipes <ajsnipes@usfca.edu>
2020-11-01 17:43:18 -0800

    finished practicing with reading from directories

--- demo.js
+++ demo.js
@@ -1,11 +1,6 @@
-//practicing reading from file
+//practicing reading directories for a file s
 var fs = require('fs')
-var data1 = require('./data.json')
-console.log(data1.name)
-    //the second parameter is the callback since is asynch
-    //here is it an annymous fuction as an arrow function but really its function
-    //(err, data)
-fs.readFile('./data.json', 'utf-8', function(err, data) {
-    var data = JSON.parse(data)
-    console.log(data.name)
+    //this allows us to read all of the directories in our folder  
+fs.readdir('./', (err, data) => {
+    console.log(data)
 })
