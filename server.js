43d0f5c9463da83a1d07268c1c5afe673deec950
Allison Snipes <ajsnipes@usfca.edu>
2020-11-01 22:05:55 -0800

    setting up server to listen

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 //practicing serving static files on express.js
+var express = require('express')
+
+// save express method as a variable
+var app = express()
+
+//sets up express server
+app.listen(3000)
