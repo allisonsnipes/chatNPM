9e1a2e2675eb2d603b906c0433a6b71978dbeb59
Allison Snipes <ajsnipes@usfca.edu>
2020-11-01 17:23:45 -0800

    finished reading from file

--- demo.js
+++ demo.js
@@ -1,2 +1,11 @@
-var _ = require('lodash')
-console.log(_.random(1, 100))
+//practicing reading from file
+var fs = require('fs')
+var data1 = require('./data.json')
+console.log(data1.name)
+    //the second parameter is the callback since is asynch
+    //here is it an annymous fuction as an arrow function but really its function
+    //(err, data)
+fs.readFile('./data.json', 'utf-8', function(err, data) {
+    var data = JSON.parse(data)
+    console.log(data.name)
+})
