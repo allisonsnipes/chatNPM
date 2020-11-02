fda490566b6c4ba8f94f0989934c1e55ab185617
Allison Snipes <ajsnipes@usfca.edu>
2020-11-02 15:47:30 -0800

    setting up the backend to talk to the front end

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var app = express()
 //need to serve static content
 app.use(express.static(__dirname))
 
+//this serves to aid in the backend to talk to the front end
+
+
 //sets up express server
 var server = app.listen(3000, () => {
     console.log('server  is listening on port', server.address().port)
