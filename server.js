078a94a3fb01bece7433d8e30d0c27eddcabe7e5
Allison Snipes <ajsnipes@usfca.edu>
2020-11-01 22:18:23 -0800

    finished static example

--- server.js
+++ server.js
@@ -4,5 +4,10 @@ var express = require('express')
 // save express method as a variable
 var app = express()
 
+//need to serve static content
+app.use(express.static(__dirname))
+
 //sets up express server
-app.listen(3000)
+var server = app.listen(3000, () => {
+    console.log('server  is listening on port', server.address().port)
+})
