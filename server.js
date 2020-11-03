efce6896494704e7524f8fb7577db098425d1661
Allison Snipes <ajsnipes@usfca.edu>
2020-11-02 18:59:36 -0800

    postman beginning setup

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 //practicing serving static files on express.js
 var express = require('express')
+var bodyParser = require('body-parser')
 
 // save express method as a variable
 var app = express()
 
 //need to serve static content
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
 
-//this serves to aid in the backend to talk to the front end
-//this will use the id identifier
+//this serves to aid in the backend to talk to the front end pulls prepopulated messages and names from the database this will use the id identifier
 app.get('/messages', (request, response) => {
     response.send(messages)
 })
 
-//placeholder message list
+//this will be in charge of getting the input from the user and saving it to the database the 200 lets the server know that everything was ok this requires Postman
+app.post('/messages', (request, response) => {
+    console.log(request.body)
+    response.sendStatus(200)
+})
+
+//placeholder message list also known as the database                                                                                                                                                                                                                          
 var messages = [
     { name: 'Bob', message: 'hi' },
     { name: 'Jane', message: 'shalom' },
